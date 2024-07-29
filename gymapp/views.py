7f3feb24d07c4ee57443f8a10eb2ca4ecc750602
Sachen-07 <160990250+Sachen-07@users.noreply.github.com>
2024-07-29 09:20:04 +0545

    update views seting about us home index

--- gymapp/views.py
+++ gymapp/views.py
@@ -1,49 +1,75 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Todo
+
 
 # Create your views here.
 def create(request):
-    if request.method == 'POST':
-        name=request.POST.get("name")
-        email=request.POST.get("email")
-        age=request.POST.get("age")
-        phone=request.POST.get("phone")
-        gender=request.POST.get("gender")
-        membership=request.POST.get("membership")
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        age = request.POST.get("age")
+        phone = request.POST.get("phone")
+        gender = request.POST.get("gender")
+        membership = request.POST.get("membership")
         print(name, email, age, phone, gender, membership)
-        Todo.objects.create(name=name, email=email, age=age, phone=phone, gender=gender, membership=membership)
-        return redirect('/members')
-    return render(request,'index.html')
+        Todo.objects.create(
+            name=name,
+            email=email,
+            age=age,
+            phone=phone,
+            gender=gender,
+            membership=membership,
+        )
+        return redirect("/members")
+    return render(request, "index.html")
+
 
 def edit(request, pk):
     todo_obj = Todo.objects.get(id=pk)
-    context={'todo':todo_obj}
-    if request.method == 'POST':
-        todo_obj.name=request.POST.get('name')
-        todo_obj.email=request.POST.get('email')
-        print('hello')
-        todo_obj.age=request.POST.get('age')
-        todo_obj.phone=request.POST.get('phone')
-        todo_obj.gender=request.POST.get('gender')
-        todo_obj.membership=request.POST.get('membership')
+    context = {"todo": todo_obj}
+    if request.method == "POST":
+        todo_obj.name = request.POST.get("name")
+        todo_obj.email = request.POST.get("email")
+        print("hello")
+        todo_obj.age = request.POST.get("age")
+        todo_obj.phone = request.POST.get("phone")
+        todo_obj.gender = request.POST.get("gender")
+        todo_obj.membership = request.POST.get("membership")
         todo_obj.save()
-        return redirect('/members')
-    return render(request,'edit.html', context)
+        return redirect("/members")
+    return render(request, "edit.html", context)
+
 
 def delete(request, pk):
-    todo_obj=Todo.objects.get(id=pk)
+    todo_obj = Todo.objects.get(id=pk)
     todo_obj.delete()
-    return redirect('members')
+    return redirect("members")
+
 
 def members(request):
-    todo_objs=Todo.objects.all()
-    context={'Todos':todo_objs}
-    return render(request,'members.html',context)
+    page = "Members"
+    todo_objs = Todo.objects.all()
+    context = {
+        "Todos": todo_objs,
+        "page": page,
+    }
+    return render(request, "members.html", context)
+
 
 def home(request):
-    return render(request,'home.html')
+    page = "Valar Dohaeris GYM"
+    context = {
+        "page": page,
+    }
+    return render(request, "home.html", context)
+
 
 # Create your views here.
 
+
 def about(request):
-    return render(request,'about-us.html')
+    page = "About us"
+    context = {
+        "page": page,
+    }
+    return render(request, "about-us.html", context)
